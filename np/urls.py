ade5cdbf21861e7a40d6590e41106336a163f4e8
caninereason <theciaranneeson@hotmail.com>
2023-03-30 17:23:42 +0100

    setup allauth

--- np/urls.py
+++ np/urls.py
@@ -1,4 +1,4 @@
-"""np URL Configuration
+"""codestar URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls'))
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls'), name='blog_urls'),
+    path("accounts/", include("allauth.urls")),
 ]
