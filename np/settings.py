52c610b61ddd11cc924cbc82bd332aafa34e4b6b
caninereason <theciaranneeson@hotmail.com>
2023-08-06 21:06:16 +0100

    change debug true/ close a tags on base.html

--- np/settings.py
+++ np/settings.py
@@ -19,7 +19,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['artspill-876968932494.herokuapp.com', 'localhost', '127.0.0.1', 'ns-incomplete.herokuapp.com',]
 
